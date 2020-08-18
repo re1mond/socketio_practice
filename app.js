4a848e3e3b5cf366aacf0c791d6de9351bc0f0be
re1mond <re1mond@outlook.com>
2020-08-18 22:41:49 +0300

    initial

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path')
-
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
@@ -14,31 +13,44 @@ app.get('/:room', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
+// Set the origins for request messagess
+// io.origins("http://localhost:8080/")
+
 io.on('connection', (socket) => {
-    console.log(chalk.bgGreen(" CONNECTION "), 
-                `User ${socket.id} connected!`)
+    console.log(chalk.bgGreen.black(" CONNECTION "), 
+                `Socket ${socket.id} connected.`)
     
-    socket.on("join-room", (roomID) => {
-        socket.join(roomID)
-        socket.to(roomID).broadcast.emit('user-joined', socket.id)    
+    socket.on("join-room", (msg) => {
+        console.log(msg)
+        socket.join(msg.room)
+        socket.to(msg.room).broadcast.emit('userJoined', msg, socket.id)    
+    })
+
+    socket.on("myData", (msg) => {
+        let { connectedId } = msg
+        delete msg.connectedId
+        socket.to(connectedId).emit('myData', msg)
+    })
+
+    socket.on("ready", (msg) => {
+        console.log(msg)
+        let { room } = msg;
+        io.to(room).emit('ready', msg.id)
     })
 
     socket.on('disconnecting', () => {
         const rooms = Object.keys(socket.rooms);
-        console.log(rooms)
-        //socket.emit('leave-group',)
+        io.to(rooms[1]).emit('chat message', "User left the chat...")
     })
 
     socket.on('chat message', (roomID, msg) => {
-        socket.to(roomID).emit('chat message', msg)
+        io.to(roomID).emit('chat message', msg)
     })
-
   });
 
 
-
 http.listen(1337, () => {
-    console.log(chalk.bgBlue("\nListening on 1337 port"))
-    console.log("http://localhost:1337/ \n")
+    console.log(chalk.bgGreen.black("\n Listening on 1337 port "))
+    console.log("\n http://localhost:1337/ \n")
 })
 
